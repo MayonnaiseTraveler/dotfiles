8fa2bf84c376549dea2cfee0f7b90fa0e91f9c83
MayonnaiseTraveler <fournandes@protonmail.com>
2022-02-21 11:27:19 -0300

    added missing files

---
+++ .config/i3/install.sh
@@ -0,0 +1,11 @@
+
+#TODO
+#download packages
+#download packages that are not in the aur such as joplin (also check if there is now a package of joplin in the aur)
+#move themes and fonts to the right places
+#set up login screen themes
+#set up obs and other stuff
+#set up browser extensions
+#install krita brushes
+#
+#
