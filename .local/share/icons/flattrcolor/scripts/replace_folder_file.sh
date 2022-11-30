d0fac79a5a412b32fdcfda0e04153132e269d194
MayonnaiseTraveler <fournandes@protonmail.com>
2022-11-30 04:00:04 -0300

    last update before potentially doing shit

--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#56222c
-newglyph=#56222c
+oldglyph=#4b1523
+newglyph=#30243d
 
 #	Front
 #	default color: 36d7b7
-oldfront=#9e3f50
-newfront=#9e3f50
+oldfront=#902944
+newfront=#5c4476
 
 #	Back
 #	default color: 1ba39c
-oldback=#6c2b37
-newback=#6c2b37
+oldback=#631c2f
+newback=#3f2f51
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
