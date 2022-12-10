2a3b6d5f675d249c93c8dc95e7cda0f91ce8db70
MayonnaiseTraveler <fournandes@protonmail.com>
2022-12-10 11:50:52 -0300

    Updated Hyprland Configs]

--- .config/hypr/import-gsettings.sh
+++ .config/hypr/import-gsettings.sh
@@ -9,6 +9,9 @@ gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
+
+hyprctl setcursor "$cursor_theme" 24
+
 gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
 gsettings set "$gnome_schema" icon-theme "$icon_theme"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
