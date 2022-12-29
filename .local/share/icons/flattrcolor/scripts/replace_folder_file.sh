785b1084deb0e35e462c544b0fd444de7c6efcf3
MayonnaiseTraveler <fournandes@protonmail.com>
2022-12-29 08:04:43 -0300

    added missing icons

--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#3d0b3b
-newglyph=#3d0b3b
+oldglyph=#456a2e
+newglyph=#456a2e
 
 #	Front
 #	default color: 36d7b7
-oldfront=#7f177b
-newfront=#7f177b
+oldfront=#78b950
+newfront=#78b950
 
 #	Back
 #	default color: 1ba39c
-oldback=#571054
-newback=#571054
+oldback=#537f37
+newback=#537f37
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
