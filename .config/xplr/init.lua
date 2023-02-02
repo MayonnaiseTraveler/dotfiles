873c0822a497d8da34e71df548561d0cab07f402
MayonnaiseTraveler <fournandes@protonmail.com>
2023-02-02 09:52:56 -0300

    updated xplr configs

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -1,24 +1,86 @@
 version = '0.20.2'
 -- switch to plugin manager later
--- local home = os.getenv("HOME")
--- local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
--- local xpm_url = "https://github.com/dtomvan/xpm.xplr"
+local home = os.getenv("HOME")
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
 
--- package.path = package.path
---   .. ";"
---   .. xpm_path
---   .. "/?.lua;"
---   .. xpm_path
---   .. "/?/init.lua"
+package.path = package.path
+  .. ";"
+  .. xpm_path
+  .. "/?.lua;"
+  .. xpm_path
+  .. "/?/init.lua"
 
--- os.execute(
---   string.format(
---     "[ -e '%s' ] || git clone '%s' '%s'",
---     xpm_path,
---     xpm_url,
---     xpm_path
---   )
--- )
+os.execute(
+  string.format(
+    "[ -e '%s' ] || git clone '%s' '%s'",
+    xpm_path,
+    xpm_url,
+    xpm_path
+  )
+)
+
+require("xpm").setup({
+	
+	-- package manager
+    'dtomvan/xpm.xplr',
+	{'sayanarijit/dual-pane.xplr',
+		setup=function()
+		require("dual-pane").setup({
+		active_pane_width = { Percentage = 70 },
+	    inactive_pane_width = { Percentage = 30 },
+		})end
+	},
+	'igorepst/context-switch.xplr',
+	-- 'sayanarijit/command-mode.xplr',
+	{'sayanarijit/wl-clipboard.xplr',
+		setup = function()
+		require("wl-clipboard").setup({
+		copy_command = "wl-copy -t text/uri-list",
+		paste_command = "wl-paste",
+		keep_selection = false,
+		})end
+	},
+	{'sayanarijit/tri-pane.xplr',
+		setup= function()
+		require('tri-pane').setup({
+			layout_key = "T", -- In switch_layout mode
+			as_default_layout = false,
+			left_pane_width = { Percentage = 20 },
+			middle_pane_width = { Percentage = 40 },
+			right_pane_width = { Percentage = 40 },
+		})end
+	},
+	{'sayanarijit/dragon.xplr',
+		setup= function()
+		require("dragon").setup({
+		  mode = "selection_ops",
+		  key = "D",
+		  drag_args = "",
+		  drop_args = "",
+		  keep_selection = false,
+		  bin = "dragon",
+		})end
+		-- Select files and type `:sD` to drag
+		-- Type `:sD` without selecting anything to drop
+	},
+	'Junker/nuke.xplr',
+
+	-- tool to decompress / compress stuffs
+	{'dtomvan/ouch.xplr',
+		setup= function()
+		require("ouch").setup({
+		  mode = "action",
+		  key = "o",
+		})end
+	},
+	'prncss-xyz/icons.xplr',
+	
+
+
+
+})
+
 
 
 xplr.config.general.panel_ui.default.border_style.fg = "Red"
@@ -133,61 +195,7 @@ xplr.config.modes.custom.bookmark = {
 }
 
 
-
-local home = os.getenv("HOME")
-package.path = home
-.. "/.config/xplr/plugins/?/init.lua;"
-.. home
-.. "/.config/xplr/plugins/?.lua;"
-.. package.path
-
-require"icons".setup()
-
-require("tri-pane").setup({
-  layout_key = "T", -- In switch_layout mode
-  as_default_layout = false,
-  left_pane_width = { Percentage = 20 },
-  middle_pane_width = { Percentage = 40 },
-  right_pane_width = { Percentage = 40 },
-  -- left_pane_renderer = custom_function_to_render_left_pane,
-  -- right_pane_renderer = custom_function_to_render_right_pane
-})
-
-
-require("dragon").setup{
-  mode = "selection_ops",
-  key = "D",
-  drag_args = "",
-  drop_args = "",
-  keep_selection = false,
-  bin = "dragon",
-}
--- Select files and type `:sD` to drag
--- Type `:sD` without selecting anything to drop
-
-require("ouch").setup{
-  mode = "action",
-  key = "o",
-}
--- tool to decompress / compress stuffs
-
-require("wl-clipboard").setup{
-  copy_command = "wl-copy -t text/uri-list",
-  paste_command = "wl-paste",
-  keep_selection = false,
-}
-
 -- Type `yy` to copy and `p` to paste files.
-
-
-require("dual-pane").setup{
-  active_pane_width = { Percentage = 70 },
-  inactive_pane_width = { Percentage = 30 },
-}
-
--- require("zentable").setup()
-require("context-switch").setup()
-require("nuke").setup()
 
 local key = xplr.config.modes.builtin.default.key_bindings.on_key
 
