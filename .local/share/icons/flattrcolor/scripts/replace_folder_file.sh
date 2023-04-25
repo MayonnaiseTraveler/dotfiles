153687968e0c10836a3b7a4d8749c853caf2126e
MayonnaiseTraveler <fournandes@protonmail.com>
2023-04-24 22:35:03 -0300

    updated icon theme

--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
 oldglyph=#640828
-newglyph=#331720
+newglyph=#6b0b2d
 
 #	Front
 #	default color: 36d7b7
 oldfront=#bc0f4c
-newfront=#6a3042
+newfront=#c51453
 
 #	Back
 #	default color: 1ba39c
 oldback=#810a34
-newback=#48212d
+newback=#870e39
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
