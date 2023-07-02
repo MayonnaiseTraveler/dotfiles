117e3d954e955ac6b4c8102fa5f240e9430073da
MayonnaiseTraveler <fournandes@protonmail.com>
2023-07-02 18:45:20 -0300

    added swaync[

--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#4e5066
-newglyph=#4e5066
+oldglyph=#3b0925
+newglyph=#3b0925
 
 #	Front
 #	default color: 36d7b7
-oldfront=#8588af
-newfront=#8588af
+oldfront=#7c144f
+newfront=#7c144f
 
 #	Back
 #	default color: 1ba39c
-oldback=#5b5e78
-newback=#5b5e78
+oldback=#550e36
+newback=#550e36
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
