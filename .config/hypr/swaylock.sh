4f89af2baf7c81a2ea6ddf877cf58e26b8880fb1
MayonnaiseTraveler <fournandes@protonmail.com>
2023-07-07 06:16:15 -0300

    updated le configs

--- .config/hypr/swaylock.sh
+++ .config/hypr/swaylock.sh
@@ -9,20 +9,20 @@ swaylock \
 --indicator-thickness 7 \
 --effect-blur 7x5 \
 --effect-vignette 0.5:0.5 \
---ring-color 231d2d \
---key-hl-color 694A5D \
---line-color 0C0A0F \
---inside-color 0C0A0F \
---separator-color 0C0A0F \
---text-color d8c3c8 \
---line-clear-color 694A5D \
---ring-clear-color d8c3c8 \
---inside-clear-color 231d2d \
---line-clear-color 231d2d \
---text-clear-color d8c3c8 \
---ring-wrong-color 995369 \
---inside-wrong-color 0C0A0F \
---text-wrong-color 995369 \
+--ring-color 2a2a2c \
+--key-hl-color 5A6D95 \
+--line-color 121213 \
+--inside-color 121213 \
+--separator-color 121213 \
+--text-color d2dce3 \
+--line-clear-color 5A6D95 \
+--ring-clear-color d2dce3 \
+--inside-clear-color 2a2a2c \
+--line-clear-color 2a2a2c \
+--text-clear-color d2dce3 \
+--ring-wrong-color 6F8BA9 \
+--inside-wrong-color 121213 \
+--text-wrong-color 6F8BA9 \
 --grace 0 \
 --font 'ShureTechMono Nerd Font Mono' \
 --fade-in 0.2
--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#252d3e
-newglyph=#271b23
+oldglyph=#271b23
+newglyph=#252d3e
 
 #	Front
 #	default color: 36d7b7
-oldfront=#485777
-newfront=#533b4a
+oldfront=#533b4a
+newfront=#485777
 
 #	Back
 #	default color: 1ba39c
-oldback=#313b51
-newback=#392833
+oldback=#392833
+newback=#313b51
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
