2e7eb8394c34ea6183f4627b675def598517fc44
MayonnaiseTraveler <fournandes@protonmail.com>
2023-08-07 00:23:39 -0300

    updated hyprland configs

--- .config/hypr/hyprland_execs_once.sh
+++ .config/hypr/hyprland_execs_once.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-touch /tmp/hyprstart.lock & 
-waybar &
-swww init &
-swww img --sync --filter Nearest --transition-duration 0 ~/Imagens/wallpaper.webp &
+touch /tmp/hyprstart.lock;
+waybar & 
+swww init;
+swww img ~/Imagens/wallpaper.webp;
+
+killall -q dunst
+while pgrep -x dunst >/dev/null; do sleep 1; done
 swaync -c .config/swaync/config.json -s .config/swaync/style.css &
 
 #clipboard
 wl-paste --type text --watch cliphist store & #Stores only text data
 wl-paste --type image --watch cliphist store & #Stores only image data
 
-mpd &
-calcurse --daemon & 
+mpd;
 mpDris2 & 
-bubblemaild &
+mpc random on
+mpc play || mpc insert Chill/Somewhere\ Nowhere\ 🌸Deep\ Lofi\ Beats.mp3 && mpc next 
+mpc toggle 
+mpc toggle
 ./xdg-portal.sh &
 nm-applet --indicator &
 obsidian & 
-syncthing-gtk --minimized & 
 librewolf &
 keepassxc &
-QT_QPA_PLATFORMTHEME=kde discord-screenaudio -style breeze &
+QT_QPA_PLATFORMTHEME=gtk3 discord-screenaudio --notify-send &
 easyeffects & 
+kitty --class=todo --single-instance -e lvim ~/TODO & 
+calcurse --daemon & 
+# bubblemaild &
+syncthing-gtk --minimized & 
+
+
