81bde96159c535b283ae65bad4cac28292c57680
MayonnaiseTraveler <fournandes@protonmail.com>
2024-01-05 18:07:05 -0300

    removed more unecessary files

--- .config/lvim/colors/colorscheme_bnnuy.lua
+++
@@ -1,7 +0,0 @@
--- Alternatively, you can provide a table specifying your colors to the setup function.
-require('base16-colorscheme').setup({
-    base00 = '#0b192d',base01 = '#c8d569',base02 = '#599297',base03 = '#4d5d81',
-    base04 = '#2a557e', base05 = '#3dd5f9', base06 = '#4d5d81', base07 = '#e4e6be',
-    base08 = '#ffffe6', base09 = '#ffff73', base0A = '#64cad3', base0B = '#246afd',
-    base0C = '#2b6eae', base0D = '#246afd', base0E = '#5673b5', base0F = '#ffffe6',
-})
--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#481e26
-newglyph=#481e26
+oldglyph=#294045
+newglyph=#444d25
 
 #	Front
 #	default color: 36d7b7
-oldfront=#8a3849
-newfront=#8a3849
+oldfront=#5f929c
+newfront=#99af54
 
 #	Back
 #	default color: 1ba39c
-oldback=#5e2732
-newback=#5e2732
+oldback=#355258
+newback=#56622f
 
-sed -i "s/#524954/$oldglyph/g" $1
-sed -i "s/#9b8aa0/$oldfront/g" $1
-sed -i "s/#716475/$oldback/g" $1
-sed -i "s/$oldglyph;/$newglyph;/g" $1
-sed -i "s/$oldfront;/$newfront;/g" $1
-sed -i "s/$oldback;/$newback;/g" $1
+sed -i "s/#178984/#294045/g" $1
+sed -i "s/#9b8aa0/#5f929c/g" $1
+sed -i "s/#716475/#355258/g" $1
+sed -i "s/#$glyphColorOriginal;/#$glyphColorNew;/g" $1
+sed -i "s/#$frontColorOriginal;/#$frontColorNew;/g" $1
+sed -i "s/#$backColorOriginal;/#$backColorNew;/g" $1
