cbca8cd080354369999175dd9812971cc6ba31a0
MayonnaiseTraveler <fournandes@protonmail.com>
2024-01-05 18:08:20 -0300

    removed more unecessary files

--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#294045
-newglyph=#444d25
+oldglyph=#10181f
+newglyph=#5a5f2f
 
 #	Front
 #	default color: 36d7b7
-oldfront=#5f929c
-newfront=#99af54
+oldfront=#263c4a
+newfront=#9faa54
 
 #	Back
 #	default color: 1ba39c
-oldback=#355258
-newback=#56622f
+oldback=#1a2933
+newback=#6d7439
 
-sed -i "s/#178984/#294045/g" $1
-sed -i "s/#9b8aa0/#5f929c/g" $1
-sed -i "s/#716475/#355258/g" $1
-sed -i "s/#$glyphColorOriginal;/#$glyphColorNew;/g" $1
-sed -i "s/#$frontColorOriginal;/#$frontColorNew;/g" $1
-sed -i "s/#$backColorOriginal;/#$backColorNew;/g" $1
+sed -i "s/#524954/$oldglyph/g" $1
+sed -i "s/#9b8aa0/$oldfront/g" $1
+sed -i "s/#716475/$oldback/g" $1
+sed -i "s/$oldglyph;/$newglyph;/g" $1
+sed -i "s/$oldfront;/$newfront;/g" $1
+sed -i "s/$oldback;/$newback;/g" $1
