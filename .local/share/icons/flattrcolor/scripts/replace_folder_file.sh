ff65e94efbf187789cfc13b3b3498d1d4252e742
MayonnaiseTraveler <fournandes@protonmail.com>
2024-02-17 00:43:50 -0300

    updated the rest of configs

--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
 oldglyph=#5a5f2f
-newglyph=#5a5f2f
+newglyph=#011a1b
 
 #	Front
 #	default color: 36d7b7
 oldfront=#9faa54
-newfront=#9faa54
+newfront=#015253
 
 #	Back
 #	default color: 1ba39c
 oldback=#6d7439
-newback=#6d7439
+newback=#013839
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
