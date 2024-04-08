1aacdba6a02a122d9a811498366fcd91d931d83d
MayonnaiseTraveler <fournandes@protonmail.com>
2024-04-08 07:44:21 -0300

    updated songinfo script to check if i'm using spotify as the main player

--- .config/ncmpcpp/songinfo.sh
+++ .config/ncmpcpp/songinfo.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+#exit if playerctl is running spotify
+if playerctl metadata | grep -m 1 -o spotify; then
+  exit 1
+fi
 
 filename="/tmp/mpd_cover.jpg"
 previewname="/tmp/mpd_preview.jpg"
