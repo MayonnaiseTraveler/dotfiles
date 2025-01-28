debec32d6ea3e36cdfebc4391dbd1a894b093788
Vivi <70679402+vivivivianne@users.noreply.github.com>
2025-01-28 00:27:54 -0300

    Update install.sh

--- gitdotfiles/scripts/install.sh
+++ gitdotfiles/scripts/install.sh
@@ -3,9 +3,8 @@
 # Will overwrite your local configs, make sure to back them up first!
 
 git clone --bare https://github.com/vivivivianne/dotfiles $HOME/.cfg
-alias dotfiles='/usr/bin/git --git-dir="$HOME/.cfg/" --work-tree="$HOME"'
-dotfiles checkout -f
-dotfiles config --local status.showUntrackedFiles no
+git --git-dir="$HOME/.cfg/" --work-tree="$HOME" checkout -f
+git --git-dir="$HOME/.cfg/" --work-tree="$HOME" config --local status.showUntrackedFiles no
 
 # Downloading the needed packages (+ some extra stuff you might not use)
 paru -S --needed $(echo ~/gitdotfiles/PC-Packages.txt)
